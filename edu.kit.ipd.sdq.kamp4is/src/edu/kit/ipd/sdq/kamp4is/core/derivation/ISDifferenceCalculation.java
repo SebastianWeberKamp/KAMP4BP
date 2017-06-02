1972f22a815330fd525d45504b1b85fe85a17b03
Sandro Koch <sandro.koch@kit.edu>
2017-06-02 10:53:50 +0200

    remove dep

--- edu.kit.ipd.sdq.kamp4is/src/edu/kit/ipd/sdq/kamp4is/core/derivation/ISDifferenceCalculation.java
+++
@@ -1,8 +0,0 @@
-package edu.kit.ipd.sdq.kamp4is.core.derivation;
-
-import edu.kit.ipd.sdq.kamp4is.core.ISArchitectureVersion;
-
-
-public class ISDifferenceCalculation extends AbstractISDifferenceCalculation<ISArchitectureVersion>{
-
-}
